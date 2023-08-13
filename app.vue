287d35d3054aae2af5c732874133f9e2442cc1ad
smitpatelx <smitbpatel1999@gmail.com>
2023-08-13 16:34:48 -0500

    Initial commit, project setup.

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-full max-h-screen bg-slate-50 flex flex-col flex-nowrap">
+    <!-- App header -->
+    <AppHeader />
+
+    <!-- Filter container -->
+    <FilterContainer />
+
+    <main class="flex-1 flex flex-col h-full overflow-y-auto">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'Noto Sans',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'Noto Color Emoji',
+        ],
+      },
+      colors: {
+        primary: '#0a192f',
+        secondary: '#0d1e36',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
